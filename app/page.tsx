d4d442bfe00ee150f5db90d631de345c70e396e1
Tharun Kumarr A <tharunkumarra@gmail.com>
2025-03-22 00:33:45 +0530

    registration form changed

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Hero from '@/components/home/Hero';
-import About from '@/components/home/About';
-import Topics from '@/components/home/Topics';
-import Eligibility from '@/components/home/Eligibility';
-import Schedule from '@/components/home/Schedule';
-import Registration from '@/components/home/RegistrationForm';
-import Contact from '@/components/home/Contact';
+import Hero from "@/components/home/Hero";
+import About from "@/components/home/About";
+import Topics from "@/components/home/Topics";
+import Eligibility from "@/components/home/Eligibility";
+import Schedule from "@/components/home/Schedule";
+import Registration from "@/components/home/Registration";
+import Contact from "@/components/home/Contact";
 
 export default function Home() {
   return (
